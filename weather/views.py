96a58b8239144e41d8c2c45d658bf93cead9f7dc
Haruto Mori <baystarsman@outlook.jp>
2023-03-05 17:48:54 +0100

    crate a index html and connect it with weather API

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+import json # this module is used to communicate with API data
+import urllib.request
+from decouple import config
 
 # Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        city = request.POST['city'] # need to have the same name as specific input tag in html
+        res = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + config('API', cast=str)).read()
+        json_data = json.loads(res)
+        data = {
+            "country_code": str(json_data['sys']['country']),
+            "coordinate": str(json_data['coord']['lon']) + ' ' + str(json_data['coord']['lat']),
+            "temp": str(json_data['main']['temp']) + 'k',
+            "pressure": str(json_data['main']['pressure']),
+            "humidity": str(json_data['main']['humidity']),
+        }
+    
+    else:
+        city = ''
+        data = {}
+
+    return render(request, 'index.html', {'city': city, 'data': data})
